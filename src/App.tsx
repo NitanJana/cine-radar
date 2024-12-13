225b65845d6bf4c37059548ca07c98da0e07de47
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2024-12-13 20:34:09 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className='bg-gray-900 h-screen flex items-center justify-center'>
+			<h1 className='text-3xl text-white font-semibold'>Initial Commit</h1>
+		</div>
+	);
+}
+
+export default App;
