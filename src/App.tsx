5a20026b823c96dcc3db9e0e4b1d070822c6bdf3
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2024-12-13 21:38:45 +0530

    setup: add tanstack query,devtools & eslint rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 function App() {
+	const queryClient = new QueryClient();
 	return (
-		<div className='bg-gray-900 h-screen flex items-center justify-center'>
-			<h1 className='text-3xl text-white font-semibold'>Initial Commit</h1>
-		</div>
+		<QueryClientProvider client={queryClient}>
+			<div className='bg-gray-900 h-screen flex items-center justify-center'>
+				<h1 className='text-3xl text-white font-semibold'>Initial Commit</h1>
+			</div>
+			<ReactQueryDevtools />
+		</QueryClientProvider>
 	);
 }
 
