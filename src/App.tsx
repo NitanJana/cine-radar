72413baca806c80cd64238ec15c2401d608f36bf
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2024-12-14 21:39:21 +0530

    setup: add react-router and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { BrowserRouter, Route, Routes } from "react-router";
+
+import Header from "./components/layout/Header";
+import { HomePage, MoviePage, PersonPage, SearchPage } from "./pages";
+
+const queryClient = new QueryClient();
 
 function App() {
-	const queryClient = new QueryClient();
 	return (
 		<QueryClientProvider client={queryClient}>
-			<div className='bg-gray-900 h-screen flex items-center justify-center'>
-				<h1 className='text-3xl text-white font-semibold'>Initial Commit</h1>
-			</div>
+			<BrowserRouter>
+				<div className='bg-gray-900 min-h-screen text-white'>
+					<Header />
+					<Routes>
+						<Route path='/' element={<HomePage />} />
+						<Route path='/movie/:id' element={<MoviePage />} />
+						<Route path='/person/:id' element={<PersonPage />} />
+						<Route path='/search' element={<SearchPage />} />
+					</Routes>
+				</div>
+			</BrowserRouter>
 			<ReactQueryDevtools />
 		</QueryClientProvider>
 	);
---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+import HomePage from "./home";
+import MoviePage from "./movie";
+import PersonPage from "./person";
+import SearchPage from "./search";
+
+export { HomePage, MoviePage, PersonPage, SearchPage };
